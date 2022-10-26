050476af90e2e51ffcb7b43719e0436b7eb82ec5
mirkomimic <mirkomimic133@gmail.com>
2022-10-26 21:57:23 +0200

    prvi

---
+++ index.php
@@ -0,0 +1,48 @@
+<?php
+require "dbBroker.php";
+
+session_start();
+
+if (isset($_POST['username']) && isset($_POST['password'])) {
+    $uname = $_POST['username'];
+    $upass = $_POST['password'];
+    // $user_id = 1;
+
+    foreach($baza as $korisnik) {
+        if($korisnik['username'] == $uname && $korisnik['password'] == $upass) {
+            $_SESSION['user_id'] = $korisnik['id'];
+            header("Location: prodavnica.php");
+            exit();
+        }
+    }
+}
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <link rel="stylesheet" type="text/css" href="css/style.css">
+    <title>Login</title>
+
+</head>
+
+<body>
+<div>
+    <div>
+        <form method="POST" action="#">
+            <div class="container">
+                <label class="username">Korisnicko ime</label>
+                <input type="text" name="username" required>
+                <br>
+                <label for="password">Lozinka</label>
+                <input type="password" name="password" required><br>
+                <button type="submit" name="submit">Prijavi se</button>
+            </div>
+        </form>
+    </div>
+</div>
+</body>
+
+</html>
---
+++ search/korpa.php
@@ -0,0 +1,55 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Korpa</title>
+</head>
+
+<body>
+
+    <h3>Proizvodi u korpi</h3>
+    <table border="1">
+        <thead>
+            <tr>
+                <th>Id</th>
+                <th>Name</th>
+                <th>Price</th>
+                <th>Add to cart</th>
+            </tr>
+        </thead>
+        <tbody>
+            <?php
+            foreach ($korpa as $pr) :
+            ?>
+                <tr>
+                    <td><?php echo $pr['id']; ?></td>
+                    <td><?php echo $pr['name']; ?></td>
+                    <td><?php echo $pr['price']; ?></td>
+                    <td>
+                        <form action="" method="post">
+                            <input type="hidden" name="id" value="<?php echo $pr['id']; ?>">
+                            <input type="submit" name="submit" value="Add">
+                        </form>
+                    </td>
+                    <td>
+                        <form action="" method="POST">
+                            <input type="hidden" name="id" value="<?php echo $pr['id']; ?>">
+                            <input type="submit" name="submit" value="remove1">
+                        </form>
+                    </td>
+                </tr>
+            <?php endforeach; ?>
+        </tbody>
+        <tfoot>
+          <td colspan="2">Ukupno: <?= $komada; ?></td>
+          <td colspan="2"><?= $ukupnoKorpa; ?></td>
+        </tfoot>
+    </table>
+    <form action="" method="POST">
+        <input type="submit" name="submit" value="remove">
+    </form>
+</body>
+</html>
